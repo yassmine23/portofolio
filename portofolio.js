ffe6a907959df029b53517461c28d6227c77fe1e
yassmine23 <yasminessam827@gmail.com>
2022-06-18 22:00:33 +0200

    first attempt

---
+++ portofolio.js
@@ -0,0 +1,48 @@
+function demo1(){
+    window.open(
+        'https://yassmine23.github.io/kerri/',
+        '_blank'
+      );
+
+}
+
+function source1(){
+    window.open(
+        'https://github.com/yassmine23/kerri',
+        '_blank'
+      );
+
+}
+
+function demo2(){
+    window.open(
+        'https://yassmine23.github.io/Movfix/',
+        '_blank'
+      );
+
+}
+
+function source2(){
+    window.open(
+        'https://github.com/yassmine23/Movfix',
+        '_blank'
+      );
+
+}
+
+
+function demo3(){
+    window.open(
+        'https://yassmine23.github.io/',
+        '_blank'
+      );
+
+}
+
+function source3(){
+    window.open(
+        'https://github.com/yassmine23/yassmine23.github.io',
+        '_blank'
+      );
+
+}
